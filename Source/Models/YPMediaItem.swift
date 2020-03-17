423dfc339481cfc33716524036d4e87e1b22207f
Cao Yuchao <taijizhu@gmail.com>
2020-03-17 22:05:20 +0800

    quick fix

--- Source/Models/YPMediaItem.swift
+++ Source/Models/YPMediaItem.swift
@@ -14,12 +14,12 @@ import Photos
 
 @objc public class YPMediaPhoto : NSObject {
     
-    public var image: UIImage { return modifiedImage ?? originalImage }
+    @objc public var image: UIImage { return modifiedImage ?? originalImage }
     public let originalImage: UIImage
     public var modifiedImage: UIImage?
     public let fromCamera: Bool
     public let exifMeta: [String: Any]?
-    public var asset: PHAsset?
+    @objc public var asset: PHAsset?
     
     public init(image: UIImage, exifMeta: [String: Any]? = nil, fromCamera: Bool = false, asset: PHAsset? = nil) {
         self.originalImage = image
